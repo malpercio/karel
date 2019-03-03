9ed78d3cf8fc115ed2cc44608e3bdad0b24075bb
Daniel González Espinoza <danielglezespinoza@gmail.com>
2019-03-03 12:49:59 -0600

    First boilerplate revision
    * Added CodeMirror dependency and its subcomponent to main component
    * Updated boilerplate with some minor tweaks
      * Added modules for typing and Vuex modules
      * Added .nvmrc with version 10

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>fas fa-external-link-alt</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid fill-height grid-list-md>
+        <v-layout>
+          <v-flex xs12 sm6> </v-flex>
+          <v-flex xs12 sm6 fill-height style="position: relative;">
+            <codemirror v-model="code" :options="cmOptions"></codemirror>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { codemirror } from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/cobalt.css";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    codemirror
+  },
+  data() {
+    return {
+      code: "",
+      cmOptions: {
+        tabSize: 4,
+        mode: "text/javascript",
+        theme: "cobalt",
+        lineNumbers: true
+      }
+    };
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style language="scss">
+.CodeMirror {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  scroll: visible;
+}
+.test {
+  height: 100%;
+  position: relative;
 }
 </style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#3f51b5",
+    secondary: "#5677fc",
+    accent: "#9c27b0",
+    error: "#ff5722",
+    info: "#2196F3",
+    success: "#259b24",
+    warning: "#ffc107"
+  },
+  iconfont: "fa"
+});
